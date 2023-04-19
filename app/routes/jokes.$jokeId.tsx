f8af610f5f62173ba8f7e24401e1811668e41dcc
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-19 14:11:24 +0530

    Delete joke

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,5 +1,8 @@
-import type { LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type {
+  ActionArgs,
+  LoaderArgs,
+} from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   isRouteErrorResponse,
   Link,
@@ -9,6 +12,7 @@ import {
 } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
+import { requireUserId } from "~/utils/session.server";
 
 export const loader = async ({ params }: LoaderArgs) => {
   const joke = await db.joke.findUnique({
@@ -22,6 +26,36 @@ export const loader = async ({ params }: LoaderArgs) => {
   return json({ joke });
 };
 
+export const action = async ({
+  params,
+  request,
+}: ActionArgs) => {
+  const form = await request.formData();
+  if (form.get("intent") !== "delete") {
+    throw new Response(
+      `The intent ${form.get("intent")} is not supported`,
+      { status: 400 }
+    );
+  }
+  const userId = await requireUserId(request);
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) {
+    throw new Response("Can't delete what does not exist", {
+      status: 404,
+    });
+  }
+  if (joke.jokesterId !== userId) {
+    throw new Response(
+      "Pssh, nice try. That's not your joke",
+      { status: 403 }
+    );
+  }
+  await db.joke.delete({ where: { id: params.jokeId } });
+  return redirect("/jokes");
+};
+
 export default function JokeRoute() {
   const data = useLoaderData<typeof loader>();
 
@@ -30,6 +64,16 @@ export default function JokeRoute() {
       <p>Here's your hilarious joke:</p>
       <p>{data.joke.content}</p>
       <Link to=".">"{data.joke.name}" Permalink</Link>
+      <form method="post">
+        <button
+          className="button"
+          name="intent"
+          type="submit"
+          value="delete"
+        >
+          Delete
+        </button>
+      </form>
     </div>
   );
 }
@@ -38,12 +82,28 @@ export function ErrorBoundary() {
   const { jokeId } = useParams();
   const error = useRouteError();
 
-  if (isRouteErrorResponse(error) && error.status === 404) {
-    return (
-      <div className="error-container">
-        Huh? What the heck is "{jokeId}"?
-      </div>
-    );
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 400) {
+      return (
+        <div className="error-container">
+          What you're trying to do is not allowed.
+        </div>
+      );
+    }
+    if (error.status === 403) {
+      return (
+        <div className="error-container">
+          Sorry, but "{jokeId}" is not your joke.
+        </div>
+      );
+    }
+    if (error.status === 404) {
+      return (
+        <div className="error-container">
+          Huh? What the heck is "{jokeId}"?
+        </div>
+      );
+    }
   }
 
   return (
