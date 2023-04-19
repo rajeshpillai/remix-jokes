462f58f218b414384eaecd3c70f750edc7dbe4e8
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-19 14:34:20 +0530

    Optimistic UI: Loader spinner

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -5,14 +5,13 @@ import type {
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import {
-  Form,
   isRouteErrorResponse,
-  Link,
   useLoaderData,
   useParams,
   useRouteError,
 } from "@remix-run/react";
 
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
 import {
   getUserId,
@@ -89,23 +88,7 @@ export default function JokeRoute() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">"{data.joke.name}" Permalink</Link>
-      {data.isOwner ? (
-        <Form method="post">
-          <button
-            className="button"
-            name="intent"
-            type="submit"
-            value="delete"
-          >
-            Delete
-          </button>
-        </Form>
-      ) : null}
-    </div>
+    <JokeDisplay isOwner={data.isOwner} joke={data.joke} />
   );
 }
 
--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -8,9 +8,11 @@ import {
   isRouteErrorResponse,
   Link,
   useActionData,
+  useNavigation,
   useRouteError,
 } from "@remix-run/react";
 
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
 import { badRequest } from "~/utils/request.server";
 import {
@@ -75,6 +77,26 @@ export const action = async ({ request }: ActionArgs) => {
 
 export default function NewJokeRoute() {
   const actionData = useActionData<typeof action>();
+  const navigation = useNavigation();
+
+  if (navigation.formData) {
+    const content = navigation.formData.get("content");
+    const name = navigation.formData.get("name");
+    if (
+      typeof content === "string" &&
+      typeof name === "string" &&
+      !validateJokeContent(content) &&
+      !validateJokeName(name)
+    ) {
+      return (
+        <JokeDisplay
+          canDelete={false}
+          isOwner={true}
+          joke={{ name, content }}
+        />
+      );
+    }
+  }
 
   return (
     <div>
