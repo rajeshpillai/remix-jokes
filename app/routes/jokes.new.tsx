d0a2772e6fe867e5110d628dab899d285a61d1e1
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-19 12:14:21 +0530

    Create new joke

--- app/routes/jokes.new.tsx
+++ app/routes/jokes.new.tsx
@@ -1,3 +1,27 @@
+import type { ActionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+import { db } from "~/utils/db.server";
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const content = form.get("content");
+  const name = form.get("name");
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (
+    typeof content !== "string" ||
+    typeof name !== "string"
+  ) {
+    throw new Error("Form not submitted correctly.");
+  }
+
+  const fields = { content, name };
+
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
