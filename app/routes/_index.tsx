efef4896486558d702449f8976e14c5e40bae722
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-19 14:19:31 +0530

    Add RSS links to home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,57 +1,32 @@
-import { json } from "@remix-run/node";
-import {
-  isRouteErrorResponse,
-  Link,
-  useLoaderData,
-  useRouteError,
-} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
-import { db } from "~/utils/db.server";
+import stylesUrl from "~/styles/index.css";
 
-export const loader = async () => {
-  const count = await db.joke.count();
-  const randomRowNumber = Math.floor(Math.random() * count);
-  const [randomJoke] = await db.joke.findMany({
-    skip: randomRowNumber,
-    take: 1,
-  });
-  if (!randomJoke) {
-    throw new Response("No random joke found", {
-      status: 404,
-    });
-  }
-  return json({ randomJoke });
-};
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
 
-export default function JokesIndexRoute() {
-  const data = useLoaderData<typeof loader>();
-
+export default function IndexRoute() {
   return (
-    <div>
-      <p>Here's a random joke:</p>
-      <p>{data.randomJoke.content}</p>
-      <Link to={data.randomJoke.id}>
-        "{data.randomJoke.name}" Permalink
-      </Link>
-    </div>
-  );
-}
-
-export function ErrorBoundary() {
-  const error = useRouteError();
-
-  if (isRouteErrorResponse(error) && error.status === 404) {
-    return (
-      <div className="error-container">
-        <p>There are no jokes to display.</p>
-        <Link to="new">Add your own</Link>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+            <li>
+              <Link reloadDocument to="/jokes.rss">
+                RSS
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-    );
-  }
-
-  return (
-    <div className="error-container">
-      I did a whoopsies.
     </div>
   );
 }
